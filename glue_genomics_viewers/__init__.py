97185dabbb833bac9c453885b4cd77632093935a
Jonathan Foster <jonathan.bruce.foster@gmail.com>
2021-09-24 16:00:03 -0400

    Fix setup for glue genome viewer as standalone

--- glue_genomics_viewers/__init__.py
+++ glue_genomics_viewers/__init__.py
@@ -1,6 +1,12 @@
+"""
+This should really by in the heatmap module
+"""
+
 def setup():
     from .heatmap.data_viewer import HeatmapViewer  # noqa
     from .heatmap.cluster_tool import ClusterTool # noqa
     #from .heatmap.heatmap_coords import HeatmapCoords #noqa
+    #from .genome_track_yo.qt import GenomeTrackViewer
     from glue.config import qt_client
     qt_client.add(HeatmapViewer)
+    #qt_client.add(GenomeTrackViewer)
