ea3bf1d2598bb3ea579e42dd33fe078242774335
Nicholas Earl <contact@nicholasearl.me>
2021-10-23 01:24:15 -0500

    Add node position tracking

--- glue_genomics_viewers/network/state.py
+++ glue_genomics_viewers/network/state.py
@@ -4,7 +4,8 @@ from glue.viewers.common.state import LayerState, ViewerState
 
 
 class NetworkLayerState(LayerState):
-    fill = CallbackProperty(False, docstring='Whether to show the markers as filled or not')
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
 
 class NetworkViewerState(ViewerState):
@@ -16,14 +17,24 @@ class NetworkViewerState(ViewerState):
         super().__init__(*args, **kwargs)
         # self._layout_helper = ComponentIDComboHelper(self, 'layout')
         self.add_callback('layout', self._on_layout_changed)
-        # self.add_callback('layers', self._on_layers_change)
+        self.add_callback('layers', self._on_layers_change)
+
+        self._node_positions = None
+
+    @property
+    def node_positions(self):
+        return self._node_positions
+
+    @node_positions.setter
+    def node_positions(self, value):
+        self._node_positions = value
 
     def _on_layers_change(self, value):
         # self.layers_data is a shortcut for
         # [layer_state.layer for layer_state in self.layers]
         # self._x_att_helper.set_multiple_data(self.layers_data)
         # self._y_att_helper.set_multiple_data(self.layers_data)
-        pass
+        print("_on_layers_change")
 
     def _on_layout_changed(self, value):
-        pass
+        print("_on_layout_changed")
